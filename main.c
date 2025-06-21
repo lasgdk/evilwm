dd5472a0448968714e5498f3949cfbecbbc538fa
Ciaran Anscomb <evilwm@6809.org.uk>
2025-06-21 09:36:34 +0100

    Remove vdeskcolumns/vdeskrows options
    Just rely on numvdesks parsing.

--- main.c
+++ main.c
@@ -57,8 +57,6 @@ static struct xconfig_option evilwm_options[] = {
 	{ XCONFIG_STRING,   "fn",           { .s = &option.font } },
 	{ XCONFIG_STRING,   "display",      { .s = &option.display } },
 	{ XCONFIG_CALL_1,   "numvdesks",    { .c1 = &set_numvdesks } },
-	{ XCONFIG_UINT,     "vdeskcolumns", { .u = &option.vdeskcolumns } },
-	{ XCONFIG_UINT,     "vdeskrows",    { .u = &option.vdeskrows } },
 	{ XCONFIG_STRING,   "fg",           { .s = &option.fg } },
 	{ XCONFIG_STRING,   "bg",           { .s = &option.bg } },
 	{ XCONFIG_STRING,   "fc",           { .s = &option.fc } },
@@ -101,9 +99,7 @@ static void helptext(void) {
 "  --bw PIXELS         window border width [" xstr(DEF_BW) "]\n"
 "  --snap PIXELS       snap distance when dragging windows [0; disabled]\n"
 "  --wholescreen       ignore monitor geometries when maximising\n"
-"  --numvdesks N       see --vdeskcolumns\n"
-"  --vdeskcolumns N    number of columns of virtual desktops [8]\n"
-"  --vdeskrows N       number of rows of virtual desktops [1]\n"
+"  --numvdesks C[xR]   logical virtual desktop geometry (columns x rows)\n"
 #ifdef SOLIDDRAG
 "  --nosoliddrag       draw outline when moving or resizing\n"
 #endif
@@ -147,8 +143,7 @@ static const char *default_options[] = {
 	"bg " DEF_BG,
 	"bw " xstr(DEF_BW),
 	"fc " DEF_FC,
-	"vdeskcolumns 8",
-	"vdeskrows 1",
+	"numvdesks 8x1",
 };
 #define NUM_DEFAULT_OPTIONS (sizeof(default_options)/sizeof(default_options[0]))
 
